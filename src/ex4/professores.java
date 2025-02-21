40af5bf2644f7ee02f98a0bf0d78d0acfeb9a59f
Eduardo Risch <rischeduardo@gmail.com>
2025-02-21 12:17:35 -0300

    tentativa ex4

---
+++ src/ex4/professores.java
@@ -0,0 +1,14 @@
+package ex4;
+public class professores {
+    int aulas;
+
+    double valorHoraAula;
+
+    String ano;
+
+    double salarioBase = aulas * 4.5 * valorHoraAula;
+
+    double adicional = salarioBase * 0.05;
+
+
+}
---
+++ src/ex4/professoresMain.java
@@ -0,0 +1,7 @@
+package ex4;
+
+public class professoresMain {
+    public static void main(String[] args) {
+        
+    }
+}
